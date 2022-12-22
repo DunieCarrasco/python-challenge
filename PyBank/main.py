b11de408152252c6232a18591199e00db36d237e
DunieCarrsco <danielcarrasco@daniels-MacBook-Pro.local>
2022-12-22 14:11:17 -0800

    new: PyBank running and printing

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,57 +1,80 @@
 import os
 import csv
-
+# file path
 budget_csv = os.path.join('/Users/danielcarrasco/Desktop/python-challenge/PyBank/Instructions/PyBank/Resources/budget_data.csv')
-
+# set up variables
 total_months = 0
 total_profit = 0
 profit_change =0
 value = 0
+greatest_increase = 0
+greatest_decrease = 0
+months_change = 0
 dates = []
 profit = []
+# define variables
 def print_percentages(budget_data):
     month_day = str(budget_data[0])
     profit_losses = int(budget_data[1])
 
 
-
+# open and read csv file
 with open(budget_csv, "r") as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=",")
         header = next(csv_reader)
-        print(header)
 
+        row_one = next(csv_reader)
+        total_months = total_months + 1
+        total_profit = int(total_profit) + int(row_one[1])
+        value = int(row_one[1])
+        
+# run loop through csv
         for row in csv_reader:
-            total_months = total_months + 1
-            total_profit = int(total_profit) + int(row[1])
-
-            value = int(row[1])
-
             dates.append(row[0])
+
+# keep track of change
             profit_change = int(row[1])-value
             profit.append(profit_change)
             value = int(row[1])
 
+# The total number of months included in the dataset
+            total_months = total_months + 1
+
+# The net total amount of "Profit/Losses" over the entire period
+            total_profit = int(total_profit) + int(row[1])
+
+# The changes in "Profit/Losses" over the entire period, and then the average of those changes
+            average_change = round(sum(profit)/len(profit),2)
+
+# The greatest increase in profits (date and amount) over the entire period
             greatest_increase = max(profit)
             greatest_value = profit.index(greatest_increase)
             greatest_date = dates[greatest_value]
 
+# The greatest decrease in profits (date and amount) over the entire period
+            greatest_decrease = min(profit)
+            lowest_value = profit.index(greatest_decrease)
+            lowest_date = dates[lowest_value]
 
-
+ 
+# print in terminal
 print("Financial Analysis")
 print('----------------------------------------')
 print (f"Total Months: {total_months}")
 print (f"Total: ${total_profit}")
-print ("Average Change: ${}")
-print (f"Greatest Increase in Profits: {greatest_date} ${greatest_increase}")
-#print (f"Greatest Decrease in Profits: {} ${}")
+print (f"Average Change: ${average_change}")
+print (f"Greatest Increase in Profits: {greatest_date} (${greatest_increase})")
+print (f"Greatest Decrease in Profits: {lowest_date} (${greatest_decrease})")
+output_file = os.path.join('/Users/danielcarrasco/Desktop/python-challenge/PyBank/Instructions/PyBank/Resources/budget_data.csv')
+
 
 
 
 
 # The total number of months included in the dataset
-total_months = 0
+
 # The net total amount of "Profit/Losses" over the entire period
-total_profit = 0
+
 
 # The changes in "Profit/Losses" over the entire period, and then the average of those changes
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,18 +1,34 @@
 import os
 import csv
 
+Total_Vote = 0
 
 election_csv = os.path.join('/Users/danielcarrasco/Desktop/python-challenge/PyPoll/Resorces/election_data.csv')
 
 election_csv = "/Users/danielcarrasco/Desktop/python-challenge/PyPoll/Resorces/election_data.csv"
 
-#read = open(election_csv, "r")
-
-#read.close()
-
 
 with open(election_csv, "r") as read:
     csv_read = csv.reader(read, delimiter= ",")
-    print(csv_read)
+    header = next(csv_read)
+    print(header)
+
     for row in csv_read:
-        print(row)
+        Total_Vote = Total_Vote + 1
+        
+
+# The total number of votes cast
+
+# A complete list of candidates who received votes
+
+# The percentage of votes each candidate won
+
+# The total number of votes each candidate won
+
+# The winner of the election based on popular vote.
+
+print(f"Election Results")
+print(f"-----------------------------")
+print(f"Total Vote: {Total_Vote}")
+print(f"-----------------------------")
+print(f"")
