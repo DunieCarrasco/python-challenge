4ec5ca342c141f97cc2f523d7c2dffa8235e9039
DunieCarrsco <danielcarrasco@daniels-MacBook-Pro.local>
2022-12-22 17:32:54 -0800

    new: PyPoll running

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,34 +1,54 @@
+# import modules
 import os
 import csv
 
-Total_Vote = 0
-
+#file path
 election_csv = os.path.join('/Users/danielcarrasco/Desktop/python-challenge/PyPoll/Resorces/election_data.csv')
 
-election_csv = "/Users/danielcarrasco/Desktop/python-challenge/PyPoll/Resorces/election_data.csv"
+# set up variables
+total_vote = 0
+candidates = []
+number_votes = []
+percent_votes = []
 
-
+# open and read csv file
 with open(election_csv, "r") as read:
     csv_read = csv.reader(read, delimiter= ",")
     header = next(csv_read)
-    print(header)
 
+    # run loop through csv
     for row in csv_read:
-        Total_Vote = Total_Vote + 1
-        
 
-# The total number of votes cast
+        # The total number of votes cast
+        total_vote = total_vote + 1
 
-# A complete list of candidates who received votes
+        # A complete list of candidates who received votes
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            number_votes.append(1)
+        else: 
+            index = candidates.index(row[2])
+            number_votes[index] = number_votes[index] + 1
 
-# The percentage of votes each candidate won
+# The percentage of votes
+for votes in number_votes:
+    percentage = (votes/total_vote) * 100
+    percentage = round((percentage),3)
+    percent_votes.append(percentage)
 
-# The total number of votes each candidate won
+    # The winner of the election based on popular vote.
+    winner = max(number_votes)
+    index = number_votes.index(winner)
+    winning_candidate = candidates[index]
 
-# The winner of the election based on popular vote.
-
+# print in terminal
 print(f"Election Results")
 print(f"-----------------------------")
-print(f"Total Vote: {Total_Vote}")
+print(f"Total Vote: {total_vote}")
 print(f"-----------------------------")
-print(f"")
+for i in range(len(candidates)):
+    print(f"{candidates[i]}: {str(percent_votes[i])}% ({str(number_votes[i])})")
+print(f"-----------------------------")
+print(f"Winner: {winning_candidate}")
+print(f"-----------------------------")
